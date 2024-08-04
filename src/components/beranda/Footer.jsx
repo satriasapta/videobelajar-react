30661ea2cb03f1be952802bfa4ca5550e7721def
Satria Sapta <satriasapta48@gmail.com>
2024-08-04 22:51:31 +0700

    fix ui footer

--- src/components/beranda/Footer.jsx
+++ src/components/beranda/Footer.jsx
@@ -1,10 +1,16 @@
 import logo from '../../assets/logo.png'
 import right from '../../assets/right.png'
+import down from '../../assets/down.png'
 import instagram from '../../assets/instagram.png'
 import facebook from '../../assets/facebook.png'
 import twitter from '../../assets/twitter.png'
 import linkedin from '../../assets/linkedin.png'
+import { useState } from 'react'
 const Footer = () => {
+    const [showKategori, setShowKategori] = useState(false)
+    const [showPerusahaan, setShowPerusahaan] = useState(false)
+    const [showKomunitas, setShowKomunitas] = useState(false)
+
     return (
         <div className="w-full bg-white pt-2 px-4 sm:p-8">
             <div className='sm:inline-flex sm:justify-between sm: w-full'>
@@ -14,30 +20,58 @@ const Footer = () => {
                     <p className='text-sm font-light text-gray-600 font-dm-sans pt-2 '>Jl. Usman Effendi No. 50 Lowokwaru, Malang</p>
                     <p className='text-sm font-light text-gray-600 font-dm-sans pt-2'>+62-877-7123-1234</p>
                 </div>
-                <div className='w-auto sm:inline-flex sm:gap-4 md:gap-8 lg:gap-12'>
-                    <div className="w-full inline-flex justify-between mt-4 sm:inline-block sm:mt-0 mb-3 ">
-                        <h2 className='text-md font-bold font-dm-sans sm:mb-2 md:w-36'> Kategori</h2>
-                        <img className='sm:hidden' src={right} alt="" />
-                        <p className='hidden sm:block text-gray-500 font-dm-sans py-1'>Digital & Teknologi</p>
-                        <p className='hidden sm:block text-gray-500 font-dm-sans py-1'>Pemasaran</p>
-                        <p className='hidden sm:block text-gray-500 font-dm-sans py-1'>Manajemen Bisnis</p>
-                        <p className='hidden sm:block text-gray-500 font-dm-sans py-1'>Desain</p>
+                <div className='w-auto sm:flex sm:justify-between sm:gap-4 md:gap-8 lg:gap-12'>
+                    <div className="w-full flex flex-col sm:flex-col justify-start mt-4 sm:mt-0 mb-3">
+                        <div className=" flex justify-between items-center">
+                            <h2 className='w-40 text-md font-bold font-dm-sans sm:mb-2'>Kategori</h2>
+                            <img
+                                className='sm:hidden cursor-pointer'
+                                src={showKategori ? down : right}
+                                alt=""
+                                onClick={() => setShowKategori(!showKategori)}
+                            />
+                        </div>
+                        <div className={`flex flex-col ${showKategori ? 'block' : 'hidden'} sm:block`}>
+                            <p className='text-gray-500 font-dm-sans py-1'>Digital & Teknologi</p>
+                            <p className='text-gray-500 font-dm-sans py-1'>Pemasaran</p>
+                            <p className='text-gray-500 font-dm-sans py-1'>Manajemen Bisnis</p>
+                            <p className='text-gray-500 font-dm-sans py-1'>Desain</p>
+                        </div>
                     </div>
 
-                    <div className="w-full inline-flex justify-between mt-4 sm:inline-block sm:mt-0 mb-3">
-                        <h2 className='text-md font-bold font-dm-sans sm:mb-2 md:w-36'> Perusahaan</h2>
-                        <img className='sm:hidden' src={right} alt="" />
-                        <p className='hidden sm:block text-gray-500 font-dm-sans py-1'>Tentang Kami</p>
-                        <p className='hidden sm:block text-gray-500 font-dm-sans py-1'>FAQ</p>
-                        <p className='hidden sm:block text-gray-500 font-dm-sans py-1'>Kebijakan Privasi</p>
-                        <p className='hidden sm:block text-gray-500 font-dm-sans py-1'>Ketentuan Layanan</p>
-                        <p className='hidden sm:block text-gray-500 font-dm-sans py-1'>Bantuan</p>
+                    <div className="w-full flex flex-col sm:flex-col justify-start mt-4 sm:mt-0 mb-3">
+                        <div className="flex justify-between items-center">
+                            <h2 className='w-40 text-md font-bold font-dm-sans sm:mb-2'>Perusahaan</h2>
+                            <img
+                                className='sm:hidden cursor-pointer'
+                                src={showPerusahaan ? down : right}
+                                alt=""
+                                onClick={() => setShowPerusahaan(!showPerusahaan)}
+                            />
+                        </div>
+                        <div className={`flex flex-col ${showPerusahaan ? 'block' : 'hidden'} sm:block`}>
+                            <p className='text-gray-500 font-dm-sans py-1'>Tentang Kami</p>
+                            <p className='text-gray-500 font-dm-sans py-1'>FAQ</p>
+                            <p className='text-gray-500 font-dm-sans py-1'>Kebijakan Privasi</p>
+                            <p className='text-gray-500 font-dm-sans py-1'>Ketentuan Layanan</p>
+                            <p className='text-gray-500 font-dm-sans py-1'>Bantuan</p>
+                        </div>
                     </div>
-                    <div className="w-full inline-flex justify-between mt-4 sm:inline-block sm:mt-0 mb-3">
-                        <h2 className='text-md font-bold font-dm-sans sm:mb-2 md:w-36'> Komunitas</h2>
-                        <img className='sm:hidden' src={right} alt="" />
-                        <p className='hidden sm:block text-gray-500 font-dm-sans py-1'>Tips Sukses</p>
-                        <p className='hidden sm:block text-gray-500 font-dm-sans py-1'>Blog</p>
+
+                    <div className="w-full flex flex-col sm:flex-col justify-start mt-4 sm:mt-0 mb-3">
+                        <div className="flex justify-between items-center">
+                            <h2 className='w-40 text-md font-bold font-dm-sans sm:mb-2'>Komunitas</h2>
+                            <img
+                                className='sm:hidden cursor-pointer'
+                                src={showKomunitas ? down : right}
+                                alt=""
+                                onClick={() => setShowKomunitas(!showKomunitas)}
+                            />
+                        </div>
+                        <div className={`flex flex-col ${showKomunitas ? 'block' : 'hidden'} sm:block`}>
+                            <p className='text-gray-500 font-dm-sans py-1'>Tips Sukses</p>
+                            <p className='text-gray-500 font-dm-sans py-1'>Blog</p>
+                        </div>
                     </div>
                 </div>
             </div>
